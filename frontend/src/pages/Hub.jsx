9e05b04e5f8754d30891c7b997ca40511e95ba29
Jesus Trejo <jestre23@yahoo.com>
2025-10-26 13:51:13 -0500

    Updated files, Basic LoginPage, & Basic CreateAccount

---
+++ frontend/src/pages/Hub.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Hub() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#000000",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#ffffff",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Hub</h1>
+      <p>Placeholder for now...</p>
+    </div>
+  );
+}
