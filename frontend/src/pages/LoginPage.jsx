b64c9105c1b0ff9b8ffd91f026e91ec88255aa1a
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2025-10-30 15:15:25 -0500

    moved some styles to css file

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -82,69 +82,28 @@ export default function LoginPage() {
       alignItems: "center", 
       height: "100vh", 
       backgroundColor: "#AAFF00"}}>
-        <div style = {{
-          backgroundColor: "white",
-          padding: "40px",
-          borderRadius: "12px",
-          boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-          width: "300px",
-          textAlign: "center",
-          }}>
-            <h1 style = {{
-              marginBottom: "20px", 
-              color: "#000"
-              }}>Login</h1>
-            <form onSubmit = {handleLogin}
-            style = {{
-              display: "flex",
-              flexDirection: "column",
-              gap: "15px",
-              }}>
-              <input type = "text"
-              placeholder = "Username"
-              value = {username}
-              onChange = {(e) => setUsername(e.target.value)}
-              required
-              style = {{
-                padding: "10px",
-                borderRadius: "8px",
-                border: "1px solid #ccc",
-              }}/>
-              <input type = "password"
-              placeholder= "Password"
-              value = {password}
-              onChange = {(e) => setPassword(e.target.value)}
-              required
-              style = {{
-                padding: "10px",
-                borderRadius: "8px",
-                border: "1px solid #ccc",
-              }}/>
-            {error && <p style={{ color: "red" }}>{error}</p>}
+        
+        <div className="loginContainer">
 
-              <button type = "submit"
-              style = {{backgroundColor: "#007bff",
-              color: "white",
-              padding: "10px",
-              border: "none",
-              borderRadius: "8px",
-              cursor: "pointer",
-              fontWeight: "bold",
-              }}>
-                Login
-              </button>
+            <h1 style={{marginBottom: "20px", color: "#000"}}>Login</h1>
+
+            <form onSubmit={handleLogin} style={{display: "flex", flexDirection: "column", gap: "15px"}}>
+              
+              <input type="text" className="textInput" placeholder="Username" value={username}
+                required onChange={(e) => setUsername(e.target.value)}/>
+
+              <input type="password" className="textInput" placeholder="Password" value={password}
+                required onChange={(e) => setPassword(e.target.value)}/>
+              
+              {error && <p style={{ color: "red" }}>{error}</p>}
+
+              <button type="submit" className="submitButton">Login</button>
             </form>
-            <button onClick = {handleCreateAccount}
-            style = {{
-              marginTop:"15px",
-              backgroundColor: "#28a745",
-              color: "white",
-              padding: "10px",
-              border: "none",
-              borderRadius: "8px",
-              cursor: "pointer",
-              fontWeight: "bold",
-            }}>
+
+            <button 
+                type="button" 
+                onClick={handleCreateAccount} 
+                className="createAccountButton">
               Create a New Account
             </button>
         </div>
