0a6ae4cab1dd43c5a983b7c45f83a16fc22db6c7
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2025-11-03 15:47:04 -0600

    extracted TextInput into its own component

---
+++ frontend/src/components/TextInput.tsx
@@ -0,0 +1,24 @@
+type Props = {
+    id: string, 
+    label: string,
+    value: string, 
+    setValue: (value: string) => void,
+    isPassword: boolean,
+}
+
+export default function TextInput(props: Props) {
+    return (
+        <div className="formTextInput">
+            <label htmlFor={props.id} className="cuprum-600">
+                {props.label}
+            </label>
+
+            <input id={props.id} 
+                type={props.isPassword ? "password" : "text"}
+                value={props.value} 
+                className="cuprum-600"
+                onChange={(e) => props.setValue(e.target.value)} 
+                required />
+        </div>
+    );
+}
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom"
 
+import TextInput from "../components/TextInput";
+
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -8,26 +10,28 @@ export default function LoginPage() {
   const [error, setError] = useState("")
   const navigate = useNavigate();
 
+
   // When page loads, check if user has valid token, if so, navigate them to hub (skipping login page). 
   // If they do not have a valid token, remove the token from local storage.
   useEffect(() => {
-  const token = localStorage.getItem("token");
-  if (!token) return;
+    const token = localStorage.getItem("token");
+    if (!token) return;
 
-  // Send token to backend for validity check.
-  fetch("http://localhost:5000/api/hub", {
-    headers: { Authorization: "Bearer " + token },
-  }) 
-    // If there is a invalid response from backend return error message.
-    .then(res => {
-      if (!res.ok) throw new Error("Invalid token");
-      return res.json();
-    })
-    // If there is a valid response navigate to hub.
-    .then(() => navigate("/hub"))
-    // On invalid response remove token from local storage.
-    .catch(() => localStorage.removeItem("token")); 
-}, [navigate]);
+    // Send token to backend for validity check.
+    fetch("http://localhost:5000/api/hub", {
+      headers: { Authorization: "Bearer " + token },
+    }) 
+      // If there is a invalid response from backend return error message.
+      .then(res => {
+        if (!res.ok) throw new Error("Invalid token");
+        return res.json();
+      })
+      // If there is a valid response navigate to hub.
+      .then(() => navigate("/hub"))
+      // On invalid response remove token from local storage.
+      .catch(() => localStorage.removeItem("token")); 
+  }, [navigate]);
+
 
   // On login button click send credentials to backend. On succesful login a token will be stored in local storage to allow users to go to hub.
   const handleLogin = (e) => {
@@ -38,22 +42,22 @@ export default function LoginPage() {
     
     // Send username and password to backend for check
     fetch("http://localhost:5000/log_in", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username: username, password: password})
-  })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: username, password: password})
+    })
     // Make response into a json file.
     .then(res => res.json())
     // If credentials are valid, token is sent, otherwise not token is sent.
     // If token exists store it locally and navigate to hub.
     // If token does not exist show login error and remove tokens just in case.
     .then(data => {
-      if (data.token){
-        localStorage.setItem("token", data.token)
-        navigate("/hub")
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        navigate("/hub");
       } 
       else {
-        setError("Username or password not found.")
+        setError("Username or password not found.");
         localStorage.removeItem("token");
       }
     })
@@ -61,35 +65,29 @@ export default function LoginPage() {
     .catch(err => console.error("Error:", err));
   };
 
+
   // Navigation to create account on button click.
   const handleCreateAccount = () => {
     navigate("/createAccount");
   }
 
 
-
   return (
     <div className="fullScreen">
       <form className="formContainer"> {/* this is a form instead of a div just so the password field doesn't complain */}
-        <h1 className="russo">LOG IN</h1>
-
-        <div className="formTextEntry">
-          <label htmlFor="username" className="cuprum-600">Username</label>
-          <input id="username" type="text" value={username} className="cuprum-600"
-            onChange={(e) => setUsername(e.target.value)} required/>
-        </div>
-
-        <div className="formTextEntry">
-          <label htmlFor="password" className="cuprum-600">Password</label>
-          <input id="password" type="password" value={password} className="cuprum-600"
-            onChange={(e) => setPassword(e.target.value)} required/>
-        </div>
         
+        <h1 className="russo">LOG IN</h1>
+        <TextInput id="username" label="Username" value={username} setValue={setUsername} />
+        <TextInput id="password" label="Password" value={password} setValue={setPassword} isPassword />
+
         {error && <p className="cuprum-600" style={{ color: "red" }}>{error}</p>}
 
         <button type="button" className="formSubmitButton cuprum-600" onClick={handleLogin}>Log In</button>
         
-        <span className="cuprum-600">Don't have an account? <a onClick={handleCreateAccount}>Create one here</a></span>
+        <span className="cuprum-600">
+          Don't have an account? <a onClick={handleCreateAccount}>Create one here</a>
+        </span>
+
       </form>
     </div>
   );
