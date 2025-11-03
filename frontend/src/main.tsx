34543ac36f8a65bb017218c3c72e44d3354547b8
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2025-11-03 11:34:17 -0600

    merged App.tsx and main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage.jsx";
+import CreateAccount from "./pages/CreateAccount.jsx";
+import Hub from "./pages/Hub.jsx";
+import Account from "./pages/AccountInfoPage.jsx";
+import Leaderboard from "./pages/Leaderboard.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
 )
+
+function App()
+{
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/hub" />} />
+                    <Route path = "/login" element = {<LoginPage />} />
+                    <Route path = "/createAccount" element = {<CreateAccount />} />
+                    <Route path = "/hub" element = {<Hub />} />
+                    <Route path = "/account" element = {<Account />} />
+                    <Route path = "/leaderboard" element = {<Leaderboard />} />
+                </Routes>
+            </Router>
+        </div>
+  );
+}
