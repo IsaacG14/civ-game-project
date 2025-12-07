ecbabcb03768103e36341489c1660483750054b1
Jesus Trejo <jestre23@yahoo.com>
2025-12-07 15:20:33 -0600

    Modified Popup

--- frontend/src/components/Popup.tsx
+++ frontend/src/components/Popup.tsx
@@ -8,33 +8,34 @@ type Props = {
     cancelText?: string;
     submitText?: string;
     hideCancel?: boolean;
-    children: ReactNode;
+    children: ReactNode | ((close: () => void) => ReactNode)
 }
 
 export default function Popup(props: Props) {
     // correctly typed ref for the <dialog> element
     const dialogRef = useRef<HTMLDialogElement | null>(null);
-
+    const close = () => { dialogRef.current?.close();}
     return (
         <dialog id={props.id} className="popup-dialog" ref={dialogRef} onClose={props.onClose}>
-            {props.title !== undefined &&
+            {props.title && 
                 <h2 className="russo" style={{marginTop:"0"}}>{props.title}</h2>
             }
             
             <img className="close-icon" src="close.svg" alt="close"
-                    onClick={() => { dialogRef.current?.close(); }}/>
+                    onClick={ close }/>
 
-            {props.children}
+            {typeof props.children === "function" 
+                ? props.children(close) : props.children}
             
             <div>
                 {!props.hideCancel && 
                     <button className="dark-button" type="button" 
-                            onClick={() => { dialogRef.current?.close(); }}>
+                            onClick={  close }>
                         {props.cancelText ?? "Cancel"}
                     </button>
                 }
                 <button className="light-button" type="button" style={{margin: "0 16px"}}
-                        onClick={() => { props.onSubmit(); dialogRef.current?.close(); }}>
+                        onClick={() => { props.onSubmit(); close(); }}>
                     {props.submitText ?? "Submit"}
                 </button>
             </div>
